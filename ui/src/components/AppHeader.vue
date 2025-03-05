66fd5e380c012d1941f105ed38d6fcba1fbe9a44
devarogundade <devarogundade@gmail.com>
2025-03-06 00:26:00 +0100

    update links

--- ui/src/components/AppHeader.vue
+++ ui/src/components/AppHeader.vue
@@ -44,8 +44,8 @@ onMounted(() => {
 
                 <nav>
                     <RouterLink to="/bridge">Bridge</RouterLink>
-                    <a href="">Devpost</a>
-                    <a href="">Github</a>
+                    <a href="https://devpost.com/software/etncall">Devpost</a>
+                    <a href="https://github.com/devarogundade/etncall">Github</a>
                 </nav>
 
                 <RouterLink to="/bridge" v-if="route.name == 'home'">
